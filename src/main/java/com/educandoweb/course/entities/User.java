d61e6598092b15cacfe1042e29d255350d46aa2c
fernando.junior <fernando.junior@informata.com.br>
2023-12-08 23:22:28 -0300

    :feature: cria tabela e entidade orders

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,6 +1,8 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +27,14 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Order> orders = new ArrayList<Order>();
+
+
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     public User() {
     }
